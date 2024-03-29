6040574c2225a8702ce4cd7af952aa20baebd87b
Sirapob PHASUT <omegaooh@gmail.com>
2024-03-29 18:34:15 +0700

    Add New Code fragment in README.md

--- README.md
+++ README.md
@@ -6,3 +6,28 @@ numpy `pip install numpy`
 pandas `pip install pandas`
 
 geopy `pip install geopy`
+
+
+### Code fragment the could be alternative of current visualizer
+```python
+import networkx as nx
+
+G = nx.DiGraph()
+for index, row in df.iterrows():
+  stop_id = row['stop_id']
+  lat = row['stop_lat']
+  lon = row['stop_lon']
+  G.add_node(stop_id, pos=(lat, lon))
+
+for index, row in df.iterrows():
+  stop_id = row['stop_id']
+  next_id = row['next_id']
+  route_id = row['route_id']
+  G.add_edge(stop_id, next_id, color=route_id)
+
+colors = nx.get_edge_attributes(G,'color').values()
+p = nx.get_node_attributes(G, 'pos')
+nx.draw(G, p, with_labels=False, node_size=50,
+        edge_color=colors)
+plt.show()
+```
--- Visualizer.py
+++ Visualizer.py
@@ -3,36 +3,18 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def visualize(filename: str, node1:str, node2:str):
-    data = pd.read_csv(filename)
+    df = pd.read_csv(filename)
+    df['color'] = df['route_id'].apply(lambda x: x.lower())
+    G = nx.from_pandas_edgelist(df, source=node1, target=node2, edge_attr=True)
+    pos = nx.spring_layout(G, k=0.2)
 
-    G = nx.DiGraph()
-
-    for stop_id in data[node1].unique():
-        G.add_node(stop_id)
-
-    for index, row in data.iterrows():
-        origin_id = row[node1]
-        to_stop_id = row[node2]
-        G.add_edge(origin_id, to_stop_id)
-
-    pos = nx.spring_layout(G)  # Use nx.circular_layout, nx.fruchterman_reingold_layout, etc. for different layouts
-
-    # Plot the graph
-    plt.figure()
-
-    # Plot nodes
-    nx.draw_networkx_nodes(G, pos, node_size=50, node_color='lightblue', edgecolors='black')
-
-    # Plot edges (adjust line width and style as needed)
-    nx.draw_networkx_edges(G, pos, width=0.2, alpha=0.7)
-
-    # Add node labels (optional)
-    nx.draw_networkx_labels(G, pos, font_size=5, font_weight='bold')
-
+    plt.figure(figsize=(10, 6))
+    nx.draw(G, pos,with_labels=True,width=0.4, node_size=150, edge_color=df['color'],font_size=5)
     plt.axis('off')  # Hide axes if not needed
     plt.title("Node and Edge graph")
     plt.show()
 
+
 if __name__ == '__main__':
     # visualize('route.csv', 'Origin', 'Destination')
-    visualize('csv_filename.csv', 'origin_id', 'dest_id')
+    visualize('fairbank_stop_line.csv', 'origin_id', 'dest_id')
